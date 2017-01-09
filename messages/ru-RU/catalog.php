0bc311158068c1dfd67612db06e4665200d39813
Dmitry Khlystov <dkhlystov@gmail.com>
2017-01-09 19:26:15 +1000

    translation added

---
+++ messages/ru-RU/catalog.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+	'Catalog' => 'Каталог',
+	'Categories' => 'Категории',
+	'Goods' => 'Товары',
+	'Create category' => 'Добавить категорию',
+	'Create goods' => 'Добавить товар',
+
+	'Changes saved successfully.' => 'Изменения успешно сохранены.',
+	'Item not found.' => 'Элемент не найден.',
+	'Item deleted successfully.' => 'Элемент успешно удален.',
+	'The value is incorrect.' => 'Значение указано неверно.',
+
+	'Active' => 'Активный',
+	'Title' => 'Заголовок',
+	'Properties' => 'Характеристики',
+	'Category' => 'Категория',
+	'Description' => 'Описание',
+	'Type' => 'Тип',
+	'Values' => 'Значения',
+	'Boolean' => 'Логический',
+	'Integer' => 'Целый',
+	'Decimal' => 'Дробный',
+	'Select' => 'Выбор',
+
+	'Add' => 'Добавтиь',
+	'Remove' => 'Удалить',
+	'OK' => 'OK',
+	'Cancel' => 'Отмена',
+	'Save' => 'Сохранить',
+	'Create' => 'Добавить',
+];
