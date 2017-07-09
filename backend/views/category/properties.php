96389e1e7d7afa7d1eecb5a4d2f479101580cce0
Dmitry Khlystov <dkhlystov@gmail.com>
2017-07-10 00:47:13 +1000

    update common properties view

--- backend/views/category/properties.php
+++ backend/views/category/properties.php
@@ -5,6 +5,7 @@ use yii\helpers\Html;
 
 use cms\catalog\backend\models\CategoryPropertyForm;
 use cms\catalog\common\models\CategoryProperty;
+use dkhlystov\widgets\ArrayInput;
 
 $title = Yii::t('catalog', 'Common properties');
 
@@ -27,43 +28,49 @@ $typesWithValues = CategoryProperty::getTypesWithValues();
 ]); ?>
 
 	<fieldset>
-		<?= $form->field($model, 'properties')->widget('dkhlystov\widgets\ArrayInput', [
-			'itemClass' => CategoryPropertyForm::className(),
-			'columns' => [
-				'name',
-				['attribute' => 'type', 'items' => CategoryProperty::getTypeNames(), 'inputOptions' => ['class' => 'form-control property-type']],
-				['attribute' => 'values', 'content' => function($model, $key, $index, $column) use ($typesWithValues) {
-					if ($model->getReadOnly())
-						return '';
+		<div class="form-group">
+			<div class="col-sm-9">
+				<?= ArrayInput::widget([
+					'model' => $model,
+					'attribute' => 'properties',
+					'itemClass' => CategoryPropertyForm::className(),
+					'columns' => [
+						'name',
+						['attribute' => 'type', 'items' => CategoryProperty::getTypeNames(), 'inputOptions' => ['class' => 'form-control property-type']],
+						['attribute' => 'values', 'content' => function($model, $key, $index, $column) use ($typesWithValues) {
+							if ($model->getReadOnly())
+								return '';
 
-					$id = Html::hiddenInput($column->getInputName($model, $index, 'id'), $model->id);
+							$id = Html::hiddenInput($column->getInputName($model, $index, 'id'), $model->id);
 
-					$name = $column->getInputName($model, $index, 'values');
-					$values = Html::hiddenInput($name, '', ['class' => 'property-values']);
-					foreach ($model->values as $value)
-						$values .= Html::hiddenInput($name . '[]', $value);
+							$name = $column->getInputName($model, $index, 'values');
+							$values = Html::hiddenInput($name, '', ['class' => 'property-values']);
+							foreach ($model->values as $value)
+								$values .= Html::hiddenInput($name . '[]', $value);
 
-					$options = ['class' => 'btn btn-default property-values'];
-					if (!in_array($model->type, $typesWithValues))
-						$options['disabled'] = true;
-					$button = Html::button($model->getAttributeLabel('values'), $options);
+							$options = ['class' => 'btn btn-default property-values'];
+							if (!in_array($model->type, $typesWithValues))
+								$options['disabled'] = true;
+							$button = Html::button($model->getAttributeLabel('values'), $options);
 
-					return $id . $values . $button;
-				}],
-				'unit',
-			],
-			'addLabel' => Yii::t('catalog', 'Add'),
-			'removeLabel' => Yii::t('catalog', 'Remove'),
-			'readOnlyAttribute' => 'readOnly',
-			'options' => [
-				'class' => 'category-properties',
-				'data-types-with-values' => $typesWithValues,
-				'data-modal-title' => Yii::t('catalog', 'Values'),
-				'data-modal-add' => Yii::t('catalog', 'Add'),
-				'data-modal-ok' => Yii::t('catalog', 'OK'),
-				'data-modal-cancel' => Yii::t('catalog', 'Cancel'),
-			],
-		]) ?>
+							return $id . $values . $button;
+						}],
+						'unit',
+					],
+					'addLabel' => Yii::t('catalog', 'Add'),
+					'removeLabel' => Yii::t('catalog', 'Remove'),
+					'readOnlyAttribute' => 'readOnly',
+					'options' => [
+						'class' => 'category-properties',
+						'data-types-with-values' => $typesWithValues,
+						'data-modal-title' => Yii::t('catalog', 'Values'),
+						'data-modal-add' => Yii::t('catalog', 'Add'),
+						'data-modal-ok' => Yii::t('catalog', 'OK'),
+						'data-modal-cancel' => Yii::t('catalog', 'Cancel'),
+					],
+				]) ?>
+			</div>
+		</div>
 	</fieldset>
 
 	<div class="form-group">
