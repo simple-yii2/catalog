72a73afb8fa146bb45ef79a12779adc53efe5431
Dmitry Khlystov <dkhlystov@gmail.com>
2017-07-17 14:52:46 +1000

    Using currencies in frontend fixed #53

--- frontend/widgets/OfferItem.php
+++ frontend/widgets/OfferItem.php
@@ -4,7 +4,10 @@ namespace cms\catalog\frontend\widgets;
 
 use Yii;
 use yii\base\Widget;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
+use cms\catalog\common\models\Currency;
+use cms\catalog\common\models\Settings;
 use cms\catalog\frontend\helpers\CatalogHelper;
 use cms\catalog\frontend\widgets\assets\OfferItemAsset;
 
@@ -16,6 +19,12 @@ class OfferItem extends Widget
 	public $options = ['class' => 'offer-item'];
 
 	protected $_url;
+
+	private static $_settings;
+
+	private static $_currency = false;
+
+	private static $_currencies;
 
 	public function init()
 	{
@@ -32,6 +41,47 @@ class OfferItem extends Widget
 		$controls = $this->renderControls($model);
 
 		echo Html::tag('div', $thumb . $caption . $controls, ['class' => $this->options]);
+	}
+
+	protected function getSettings()
+	{
+		if (self::$_settings !== null)
+			return self::$_settings;
+
+		$settings = Settings::find()->one();
+		if ($settings === null)
+			$settings = new Settings;
+
+		return self::$_settings = $settings;
+	}
+
+	protected function getCurrency($id)
+	{
+		//init currencies if needed
+		if (self::$_currencies === null) {
+			$items = [];
+			foreach (Currency::find()->all() as $item) {
+				$items[$item->id] = $item;
+			}
+			self::$_currencies = $items;
+		}
+
+		return ArrayHelper::getValue(self::$_currencies, $id);
+	}
+
+	protected function getApplicationCurrency()
+	{
+		if (self::$_currency !== false)
+			return self::$_currency;
+
+		//from component
+		$component = Yii::$app->get('currency', false);
+		if ($component !== null)
+			return self::$_currency = $this->getCurrency($component->currency_id);
+
+		//from settings
+		$settings = $this->getSettings();
+		return self::$_currency = $this->getCurrency($settings->defaultCurrency_id);
 	}
 
 	protected function createUrl($model)
@@ -70,21 +120,50 @@ class OfferItem extends Widget
 	protected function renderControls($model)
 	{
 		$formatter = Yii::$app->getFormatter();
+		$currency = $this->getCurrency($model->currency_id);
 
 		//old price
 		$s = '';
 		if (!empty($model->oldPrice))
-			$s = Html::tag('s', $formatter->asDecimal($model->oldPrice));
+			$s = $this->renderPrice('s', $model->oldPrice, $currency);
 		$oldPrice = Html::tag('div', $s, ['class' => 'offer-old-price']);
 
 		//price
-		$s = Html::tag('span', $formatter->asDecimal($model->price));
+		$s = $this->renderPrice('span', $model->price, $currency);
 		$price = Html::tag('div', $s, ['class' => 'offer-price']);
+
 
 		//buttons
 		$buttons = $available = '';
 
 		return Html::tag('div', $oldPrice . $price . $buttons . $available, ['class' => 'offer-controls']);
+	}
+
+	protected function renderPrice($tag, $value, $currency = null)
+	{
+		$formatter = Yii::$app->getFormatter();
+		$appCurrency = $this->getApplicationCurrency();
+
+		//calc
+		if ($appCurrency !== null && $currency !== null && $appCurrency->id != $currency->id)
+			$value = $value * $currency->rate / $appCurrency->rate;
+
+		//format
+		$r = Html::tag($tag, $formatter->asDecimal($value));
+
+		//prefix/suffix
+		if ($currency !== null) {
+			$c = $appCurrency;
+			if ($c === null)
+				$c = $currency;			
+
+			if (!empty($c->prefix))
+				$r = Html::encode($c->prefix) . '&nbsp;' . $r;
+			if (!empty($c->suffix))
+				$r .= '&nbsp;' . Html::encode($c->suffix);
+		}
+
+		return $r;
 	}
 
 }
