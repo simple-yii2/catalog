db81d3d82ab794ea575629532f0fba44f46d57be
Dmitry Khlystov <dkhlystov@gmail.com>
2018-03-17 21:32:07 +1000

    Backend module refactoring

--- backend/Module.php
+++ backend/Module.php
@@ -3,141 +3,118 @@
 namespace cms\catalog\backend;
 
 use Yii;
-
+use cms\components\BackendModule;
 use cms\catalog\common\models\Category;
 
-class Module extends \yii\base\Module {
+class Module extends BackendModule
+{
 
-	/**
-	 * @var integer;
-	 */
-	public $vendorThumbWidth = 100;
+    /**
+     * @var integer;
+     */
+    public $vendorThumbWidth = 100;
 
-	/**
-	 * @var integer;
-	 */
-	public $vendorThumbHeight = 100;
+    /**
+     * @var integer;
+     */
+    public $vendorThumbHeight = 100;
 
-	/**
-	 * @var integer;
-	 */
-	public $offerThumbWidth = 360;
+    /**
+     * @var integer;
+     */
+    public $offerThumbWidth = 360;
 
-	/**
-	 * @var integer;
-	 */
-	public $offerThumbHeight = 270;
+    /**
+     * @var integer;
+     */
+    public $offerThumbHeight = 270;
 
-	/**
-	 * @var boolean
-	 */
-	public $vendorEnabled = true;
+    /**
+     * @var boolean
+     */
+    public $vendorEnabled = true;
 
-	/**
-	 * @var boolean
-	 */
-	public $barcodeEnabled = true;
+    /**
+     * @var boolean
+     */
+    public $barcodeEnabled = true;
 
-	/**
-	 * @var boolean
-	 */
-	public $propertiesEnabled = true;
+    /**
+     * @var boolean
+     */
+    public $propertiesEnabled = true;
 
-	/**
-	 * @var boolean
-	 */
-	public $storeEnabled = true;
+    /**
+     * @var boolean
+     */
+    public $storeEnabled = true;
 
-	/**
-	 * @var integer|null
-	 */
-	public $maxCategoryDepth = null;
+    /**
+     * @var integer|null
+     */
+    public $maxCategoryDepth = null;
 
-	/**
-	 * @inheritdoc
-	 */
-	public function init()
-	{
-		parent::init();
+    /**
+     * @inheritdoc
+     */
+    public static function moduleName()
+    {
+        return 'catalog';
+    }
 
-		$this->checkDatabase();
-		self::addTranslation();
-	}
+    /**
+     * @inheritdoc
+     */
+    protected static function cmsDatabase()
+    {
+        parent::cmsDatabase();
 
-	/**
-	 * Database checking
-	 * @return void
-	 */
-	protected function checkDatabase()
-	{
-		//schema
-		$db = Yii::$app->db;
-		$filename = dirname(__DIR__) . '/schema/' . $db->driverName . '.sql';
-		$sql = explode(';', file_get_contents($filename));
-		foreach ($sql as $s) {
-			if (trim($s) !== '')
-				$db->createCommand($s)->execute();
-		}
+        if (Category::find()->roots()->count() == 0) {
+            $root = new Category(['title' => 'Root']);
+            $root->makeRoot();
+        }
+    }
 
-		//rbac
-		$auth = Yii::$app->getAuthManager();
-		if ($auth->getRole('Catalog') === null) {
-			//role
-			$role = $auth->createRole('Catalog');
-			$auth->add($role);
-		}
+    /**
+     * @inheritdoc
+     */
+    protected static function cmsSecurity()
+    {
+        parent::cmsSecurity();
 
-		//data
-		if (Category::find()->roots()->count() == 0) {
-			$root = new Category(['title' => 'Root']);
-			$root->makeRoot();
-		}
-	}
+        $auth = Yii::$app->getAuthManager();
+        if ($auth->getRole('Catalog') === null) {
+            //role
+            $role = $auth->createRole('Catalog');
+            $auth->add($role);
+        }
+    }
 
-	/**
-	 * Adding translation to i18n
-	 * @return void
-	 */
-	protected static function addTranslation()
-	{
-		if (!isset(Yii::$app->i18n->translations['catalog'])) {
-			Yii::$app->i18n->translations['catalog'] = [
-				'class' => 'yii\i18n\PhpMessageSource',
-				'sourceLanguage' => 'en-US',
-				'basePath' => dirname(__DIR__) . '/messages',
-			];
-		}
-	}
+    /**
+     * @inheritdoc
+     */
+    public function cmsMenu($base)
+    {
+        if (!Yii::$app->getUser()->can('Catalog')) {
+            return [];
+        }
 
-	/**
-	 * Making main menu item of module
-	 * @param string $base route base
-	 * @return array
-	 */
-	public function cmsMenu($base)
-	{
-		self::addTranslation();
+        $items = [];
+        $items[] = ['label' => Yii::t('catalog', 'Settings'), 'url' => ["$base/catalog/settings/index"]];
+        $items[] = ['label' => Yii::t('catalog', 'Currencies'), 'url' => ["$base/catalog/currency/index"]];
+        if ($this->vendorEnabled) {
+            $items[] = ['label' => Yii::t('catalog', 'Vendors'), 'url' => ["$base/catalog/vendor/index"]];
+        }
+        if ($this->storeEnabled) {
+            $items[] = ['label' => Yii::t('catalog', 'Stores'), 'url' => ["$base/catalog/store/index"]];
+        }
+        $items[] = '<li role="separator" class="divider"></li>';
+        $items[] = ['label' => Yii::t('catalog', 'Categories'), 'url' => ["$base/catalog/category/index"]];
+        $items[] = ['label' => Yii::t('catalog', 'Goods/Services'), 'url' => ["$base/catalog/offer/index"]];
 
-		$items = [];
-		$items[] = ['label' => Yii::t('catalog', 'Settings'), 'url' => ["$base/catalog/settings/index"]];
-		$items[] = ['label' => Yii::t('catalog', 'Currencies'), 'url' => ["$base/catalog/currency/index"]];
-		if ($this->vendorEnabled) {
-			$items[] = ['label' => Yii::t('catalog', 'Vendors'), 'url' => ["$base/catalog/vendor/index"]];
-		}
-		if ($this->storeEnabled) {
-			$items[] = ['label' => Yii::t('catalog', 'Stores'), 'url' => ["$base/catalog/store/index"]];
-		}
-		$items[] = '<li role="separator" class="divider"></li>';
-		$items[] = ['label' => Yii::t('catalog', 'Categories'), 'url' => ["$base/catalog/category/index"]];
-		$items[] = ['label' => Yii::t('catalog', 'Goods/Services'), 'url' => ["$base/catalog/offer/index"]];
-
-		if (Yii::$app->user->can('Catalog')) {
-			return [
-				['label' => Yii::t('catalog', 'Catalog'), 'items' => $items],
-			];
-		}
-		
-		return [];
-	}
+        return [
+            ['label' => Yii::t('catalog', 'Catalog'), 'items' => $items],
+        ];
+    }
 
 }
