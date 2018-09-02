da68bce994e63d52647934affee0892c16d3f3f0
Dmitry Khlystov <dkhlystov@gmail.com>
2018-09-03 00:10:36 +1000

    Category refactoring

--- common/models/Category.php
+++ common/models/Category.php
@@ -13,191 +13,236 @@ use creocoder\nestedsets\NestedSetsQueryBehavior;
 class Category extends ActiveRecord
 {
 
-	const ALIAS_SEPARATOR = '/';
-	const PATH_SEPARATOR = ' » ';
+    const ALIAS_SEPARATOR = '/';
+    const PATH_SEPARATOR = ' » ';
 
-	/**
-	 * @var Category[]
-	 */
-	private $_parents;
+    /**
+     * @var Category[]
+     */
+    private $_parents;
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function tableName()
-	{
-		return 'catalog_category';
-	}
+    /**
+     * @var array
+     */
+    private $_activeChildren;
 
-	/**
-	 * @inheritdoc
-	 * Default values
-	 */
-	public function __construct($config = [])
-	{
-		parent::__construct(array_replace([
-			'active' => true,
-			'productCount' => 0,
-		], $config));
-	}
+    /**
+     * @var integer
+     */
+    private $_totalProductCount;
 
-	/**
-	 * Properties relation
-	 * @return ActiveQuery
-	 */
-	public function getProperties()
-	{
-		return $this->hasMany(CategoryProperty::className(), ['category_id' => 'id']);
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'catalog_category';
+    }
 
-	/**
-	 * Products relation
-	 * @return ActiveQuery
-	 */
-	public function getProducts()
-	{
-		return $this->hasMany(Product::className(), ['category_id' => 'id'])->inverseOf('category');
-	}
+    /**
+     * @inheritdoc
+     * Default values
+     */
+    public function __construct($config = [])
+    {
+        parent::__construct(array_replace([
+            'active' => true,
+            'productCount' => 0,
+        ], $config));
+    }
 
-	/**
-	 * Parents getter
-	 * @return Category[]
-	 */
-	public function getParents()
-	{
-		if ($this->_parents !== null)
-			return $this->_parents;
+    /**
+     * Properties relation
+     * @return ActiveQuery
+     */
+    public function getProperties()
+    {
+        return $this->hasMany(CategoryProperty::className(), ['category_id' => 'id']);
+    }
 
-		return $this->_parents = $this->parents()->all();
-	}
+    /**
+     * Products relation
+     * @return ActiveQuery
+     */
+    public function getProducts()
+    {
+        return $this->hasMany(Product::className(), ['category_id' => 'id'])->inverseOf('category');
+    }
 
-	/**
-	 * Parent properties
-	 * @return CategoryProperty[]
-	 */
-	public function getParentProperties()
-	{
-		$items = [];
-		foreach ($this->getParents() as $parent) {
-			$items = array_merge($items, $parent->properties);
-		}
+    /**
+     * Parents getter
+     * @return Category[]
+     */
+    public function getParents()
+    {
+        if ($this->_parents !== null)
+            return $this->_parents;
 
-		array_walk($items, function(&$item) {
-			$item->readOnly = true;
-		});
+        return $this->_parents = $this->parents()->all();
+    }
 
-		return $items;
-	}
+    /**
+     * Parent properties
+     * @return CategoryProperty[]
+     */
+    public function getParentProperties()
+    {
+        $items = [];
+        foreach ($this->getParents() as $parent) {
+            $items = array_merge($items, $parent->properties);
+        }
 
-	/**
-	 * Find by alias
-	 * @param sring $alias Alias or id
-	 * @return static
-	 */
-	public static function findByAlias($alias) {
-		$model = static::findOne(['alias' => $alias]);
-		if ($model === null)
-			$model = static::findOne(['id' => $alias]);
+        array_walk($items, function(&$item) {
+            $item->readOnly = true;
+        });
 
-		return $model;
-	}
+        return $items;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function behaviors()
-	{
-		return [
-			'tree' => [
-				'class' => NestedSetsBehavior::className(),
-			],
-		];
-	}
+    /**
+     * Find by alias
+     * @param sring $alias Alias or id
+     * @return static
+     */
+    public static function findByAlias($alias) {
+        $model = static::findOne(['alias' => $alias]);
+        if ($model === null)
+            $model = static::findOne(['id' => $alias]);
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function find()
-	{
-		return new CategoryQuery(get_called_class());
-	}
+        return $model;
+    }
 
-	/**
-	 * Making alias and path from title and parent alias and path
-	 * @param Category|null $parent 
-	 * @return void
-	 */
-	public function makeAliasAndPath(Category $parent = null)
-	{
-		if ($this->isRoot()) {
-			$this->alias = null;
-			$this->path = null;
-			return;
-		}
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'tree' => [
+                'class' => NestedSetsBehavior::className(),
+            ],
+        ];
+    }
 
-		if ($parent === null)
-			$parent = $this->parents(1)->one();
+    /**
+     * @inheritdoc
+     */
+    public static function find()
+    {
+        return new CategoryQuery(get_called_class());
+    }
 
-		$alias = '';
-		$path = '';
+    /**
+     * Making alias and path from title and parent alias and path
+     * @param Category|null $parent 
+     * @return void
+     */
+    public function makeAliasAndPath(Category $parent = null)
+    {
+        if ($this->isRoot()) {
+            $this->alias = null;
+            $this->path = null;
+            return;
+        }
 
-		if ($parent !== null && !$parent->isRoot()) {
-			$alias = $parent->alias;
-			$path = $parent->path;
-		}
+        if ($parent === null)
+            $parent = $this->parents(1)->one();
 
-		if (!empty($alias))
-			$alias .= self::ALIAS_SEPARATOR;
-		if (!empty($path))
-			$path .= self::PATH_SEPARATOR;
+        $alias = '';
+        $path = '';
 
-		$this->alias = $alias . Translit::t($this->title);
-		$this->path = $path . $this->title;
-	}
+        if ($parent !== null && !$parent->isRoot()) {
+            $alias = $parent->alias;
+            $path = $parent->path;
+        }
 
-	/**
-	 * Update alias and path with children
-	 * @param Category|null $parent 
-	 * @return void
-	 */
-	public function updateAliasAndPath(Category $parent = null)
-	{
-		$this->makeAliasAndPath($parent);
-		$this->update(false, ['alias', 'path']);
+        if (!empty($alias))
+            $alias .= self::ALIAS_SEPARATOR;
+        if (!empty($path))
+            $path .= self::PATH_SEPARATOR;
 
-		if ($this->isLeaf())
-			return;
+        $this->alias = $alias . Translit::t($this->title);
+        $this->path = $path . $this->title;
+    }
 
-		foreach ($this->children(1)->all() as $object)
-			$object->updateAliasAndPath($this);
-	}
+    /**
+     * Update alias and path with children
+     * @param Category|null $parent 
+     * @return void
+     */
+    public function updateAliasAndPath(Category $parent = null)
+    {
+        $this->makeAliasAndPath($parent);
+        $this->update(false, ['alias', 'path']);
 
-	/**
-	 * Update product count
-	 * @return void
-	 */
-	public function updateProductCount()
-	{
-		$count = $this->getProducts()->count();
-		if ($count != $this->productCount) {
-			$this->productCount = $count;
-			$this->update(false, ['productCount']);
-		}
-	}
+        if ($this->isLeaf())
+            return;
+
+        foreach ($this->children(1)->all() as $object)
+            $object->updateAliasAndPath($this);
+    }
+
+    /**
+     * Update product count
+     * @return void
+     */
+    public function updateProductCount()
+    {
+        $count = $this->getProducts()->count();
+        if ($count != $this->productCount) {
+            $this->productCount = $count;
+            $this->update(false, ['productCount']);
+        }
+    }
+
+    /**
+     * Active sub categories with products
+     * @return array
+     */
+    public function getActiveChildren()
+    {
+        if ($this->_activeChildren !== null) {
+            return $this->_activeChildren;
+        }
+
+        $children = [];
+        $item = null;
+        $skip = 0;
+        foreach ($this->children()->all() as $value) {
+            if ($value->depth == $this->depth + 1) {
+                if ($item !== null && $item->active && $item->productCount > 0) {
+                    $children[] = $item;
+                }
+                $item = $value;
+            }
+            if (!$value->active) {
+                $skip = $value->rgt;
+            }
+            if ($value->rgt <= $skip) {
+                continue;
+            }
+            $item->productCount += $value->productCount;
+        }
+        if ($item !== null && $item->active && $item->productCount > 0) {
+            $children[] = $item;
+        }
+
+        return $this->_activeChildren = $children;
+    }
 
 }
 
 class CategoryQuery extends ActiveQuery
 {
 
-	/**
-	 * @inheritdoc
-	 */
-	public function behaviors()
-	{
-		return [
-			NestedSetsQueryBehavior::className(),
-		];
-	}
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            NestedSetsQueryBehavior::className(),
+        ];
+    }
 
 }
